66838e05f66f3ef6cf41d99baec8d629f9d25195
RichardDev01 <richard.jansen@student.hu.nl>
2020-02-05 10:21:07 +0100

    added inverse piramide

--- Opdracht 1/PyramideWhileloop.py
+++ Opdracht 1/PyramideWhileloop.py
@@ -1,19 +1,41 @@
 def makepiramidefirsthalf(hight,base):
     while base < hight:
-        print(base * "*")
+        string = base * "*"
+        print(string)
+        #print('{:>20}'.format(string))
         base += 1
 
     return 0
 
 def makepiramidesecondhalf(hight,base):
     while base < hight:
-        print(hight*"*")
+        string = hight * "*"
+        print(string)
+        hight-=1
+    return
+
+def makepiramidefirsthalfin(hight,base):
+    while base < hight:
+        string = base * "*"
+        print('{:>20}'.format(string))
+        base += 1
+
+    return 0
+
+def makepiramidesecondhalfin(hight,base):
+    while base < hight:
+        string = hight * "*"
+
+        print('{:>20}'.format(string))
         hight-=1
     return
 
 def makepiramide(hight):
     makepiramidefirsthalf(hight,0)
     makepiramidesecondhalf(hight,0)
+    makepiramidefirsthalfin(hight,0)
+    makepiramidesecondhalfin(hight,0)
+
     return 0
 
 
