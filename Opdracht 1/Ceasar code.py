ffd864a5d8e3f40786c7cc3132226cc593812695
RichardDev01 <richard.jansen@student.hu.nl>
2020-02-05 15:04:47 +0100

    Alle opdrachten toegevoegt en randomgemaakt

---
+++ Opdracht 1/Ceasar code.py
@@ -0,0 +1,7 @@
+'''
+Caesarcijfer
+Schrijf een programma voor Caesarcijfers. Voorbeeld van de interactie met het programma:
+
+
+nog maken
+'''
---
+++ Opdracht 1/Random.py
@@ -0,0 +1,11 @@
+'''
+Random
+Schrijf een programma dat een willekeurig getal kiest en de gebruiker net zo lang laat gokken tot dat ze het goed hebben.
+'''
+import random
+number = random.randrange(10)
+while True:
+    gues = int(input("kies een getoal tussen 0 en 10:  "))
+    if gues == number:
+        print("goodjob")
+        break
