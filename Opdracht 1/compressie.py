a6a1f9e435c8b6c2ceb267665a68e50a6cc9e743
RichardDev01 <richard.jansen@student.hu.nl>
2020-02-06 13:46:33 +0100

    Compressie opdracht gemaakt

--- Opdracht 1/compressie.py
+++ Opdracht 1/compressie.py
@@ -2,8 +2,6 @@
 Compressie
 Schrijf een compress-programma, dat uit een gegeven bestand een nieuwe bestand maakt, waarbij van iedere regel alle spaties en tabs aan het begin van de regel zijn verwijderd.
 Verder zijn alle lege regels verwijderd (een lege regel bevat ’\n’ , eventueel voorafgegaan door spaties en tabs(‘\t’)).
-
-nog maken
 '''
 filename = "compressie.txt"
 filenametarget = "compressietarget.txt"
