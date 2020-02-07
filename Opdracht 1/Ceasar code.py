b4bcd62cad8b9ed319255dda0ab5a2f1ef8a942b
RichardDev01 <richard.jansen@student.hu.nl>
2020-02-07 14:46:04 +0100

    fibonaci met recursie

--- Opdracht 1/Ceasar code.py
+++ Opdracht 1/Ceasar code.py
@@ -2,6 +2,13 @@
 Caesarcijfer
 Schrijf een programma voor Caesarcijfers. Voorbeeld van de interactie met het programma:
 
-
 nog maken
 '''
+
+alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+mystring = "hallo"
+
+#print(alphabet[29%26])
+n=3
+for i in mystring:
+    print(alphabet[alphabet.index(i)+n%26], end="")
