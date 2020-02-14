16136cf7cf58897dde57a14b7bc6f71d5a290891
RichardDev01 <richard.jansen@student.hu.nl>
2020-02-14 15:15:45 +0100

    AI werkend op eerste 2 ronden met for loops, ongeveer 360 opties blijven dan nog over

--- Codebreakermastermind/GeheugenbankAIPlusAIGokken.py
+++ Codebreakermastermind/GeheugenbankAIPlusAIGokken.py
@@ -5,7 +5,7 @@ https://docs.python.org/2/library/itertools.html
 '''
 
 import  itertools
-
+import random
 def ControlleCode(gegokte_code):
     Blackpin = 0
     Whitepin =0
@@ -22,21 +22,74 @@ def ControlleCode(gegokte_code):
             continue
     return [Blackpin,Whitepin]
 
+def ControlleCodeAIBrain(gegokte_code):
+    Blackpin = 0
+    Whitepin =0
+    for i in range (len(gegokte_code)):
+        if gegokte_code[i]==AIGokCodeComparator[i]:
+            Blackpin+=1
+            continue
+    for i in range(len(gegokte_code)):
+        if gegokte_code[i] in AIGokCodeComparator:
+            if AIGokCodeComparator.count(gegokte_code[i])>  Blackpin:
+                Whitepin += 1
+            else:
+                continue
+            continue
+    return [Blackpin,Whitepin]
+
 
 GlobalGamemastercode= ['groen', 'zwart', 'paars', 'paars']
 GlobalGokcode = []
 GlobalAwsPin = []
 
 ListKleurenNamen = ["zwart", "wit","groen","blauw","paars","rood"]
-DefaultGokCode = [ListKleurenNamen[4], ListKleurenNamen[0], ListKleurenNamen[0], ListKleurenNamen[4]]
+DefaultGokCode = [ListKleurenNamen[0], ListKleurenNamen[0], ListKleurenNamen[1], ListKleurenNamen[1]]
+
+AIGokCodeComparator= DefaultGokCode
 result = itertools.product(ListKleurenNamen, repeat=4)
 
 mainlist = []
+AIChoiceslist= []
+AIChoiceslist2= []
 for item in result:
     #print(item[0:4])
     mainlist.append(list(item))
+
+##Ronde 1
 print("Kraak",GlobalGamemastercode)
 print("Gok  ",DefaultGokCode)
-print(ControlleCode(DefaultGokCode))
+GokUitkomst = ControlleCode(DefaultGokCode)
+print(GokUitkomst)
 
+for i in range(len(mainlist)):
+    #print(mainlist[i],end= " ")
+    AITestuitkomst = ControlleCodeAIBrain(mainlist[i])
+    #print(ControlleCodeAIBrain(mainlist[i]))
+    if AITestuitkomst[0] >= GokUitkomst[0]:
+        AIChoiceslist.append(mainlist[i])
+
+
+#ronde 2
+
+AIGokCodeComparator = AIChoiceslist[random.randrange(len(AIChoiceslist))]
+print("Kraak",GlobalGamemastercode)
+print("Gok  ",AIGokCodeComparator)
+GokUitkomst = ControlleCode(AIGokCodeComparator)
+print(GokUitkomst)
+
+for i in range(len(AIChoiceslist)):
+    #print(mainlist[i],end= " ")
+    AITestuitkomst = ControlleCodeAIBrain(AIChoiceslist[i])
+    #print(ControlleCodeAIBrain(mainlist[i]))
+    if AITestuitkomst[0] >= GokUitkomst[0]:
+        AIChoiceslist2.append(AIChoiceslist[i])
+
+for j in range(len(AIChoiceslist2)):
+    print(AIChoiceslist2[j])
+#print(AIChoiceslist2)
+
+#for j in range(len(AIChoiceslist)):
+#    print(AIChoiceslist[j])
+#print(AIChoiceslist)
 
