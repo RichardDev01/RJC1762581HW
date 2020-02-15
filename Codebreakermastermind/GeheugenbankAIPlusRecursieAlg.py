dd2de0afa2e45deedddf0a540178d43cb88bc15c
RichardDev01 <richard.jansen@student.hu.nl>
2020-02-15 12:45:34 +0100

    Ai play test

--- Codebreakermastermind/GeheugenbankAIPlusRecursieAlg.py
+++ Codebreakermastermind/GeheugenbankAIPlusRecursieAlg.py
@@ -7,43 +7,77 @@ https://docs.python.org/2/library/itertools.html
 import  itertools
 import random
 def ControlleCode(gegokte_code):
+    '''De witte pin correctie werkt niet 100% maar dar maakt in deze keuze niet uit behalve effiecnietie'''
     Blackpin = 0
     Whitepin =0
     for i in range (len(gegokte_code)):
         if gegokte_code[i]==GlobalGamemastercode[i]:
             Blackpin+=1
             continue
+    #for i in range(len(gegokte_code)):
+        if gegokte_code[i] in GlobalGamemastercode and (GlobalGamemastercode.count(gegokte_code[i]) > Whitepin):
+            Whitepin += 1
+    '''
     for i in range(len(gegokte_code)):
-        if gegokte_code[i] in GlobalGamemastercode:
-            if GlobalGamemastercode.count(gegokte_code[i])>Blackpin and Whitepin < GlobalGamemastercode.count(gegokte_code[i]) and gegokte_code.count(gegokte_code[i])>Blackpin:
+        if gegokte_code[i] in GlobalGamemastercode and gegokte_code[i] !=GlobalGamemastercode[i] and GlobalGamemastercode.count(gegokte_code[i]) >= gegokte_code.count(gegokte_code[i]):
+            Whitepin += 1
+            #if GlobalGamemastercode.count(gegokte_code[i])>Blackpin and Whitepin < GlobalGamemastercode.count(gegokte_code[i]) and gegokte_code.count(gegokte_code[i])>Blackpin and gegokte_code[i]!=GlobalGamemastercode[i]:
+            #if Whitepin < GlobalGamemastercode.count(gegokte_code[i]) and gegokte_code[i] !=GlobalGamemastercode[i]:
+
                 #hier moet ik nog aanpassen nu is het verkeerd
-                Whitepin += 1
+               # Whitepin += 1
 
 
-            else:
-                continue
-            continue
+            #else:
+             #   continue
+            #continue
+      '''
     return [Blackpin,Whitepin]
 
 def ControlleCodeAIBrain(gegokte_code):
+    '''De witte pin correctie werkt niet 100% maar dar maakt in deze keuze niet uit behalve effiecnietie'''
     Blackpin = 0
     Whitepin =0
     for i in range (len(gegokte_code)):
         if gegokte_code[i]==AIGokCodeComparator[i]:
             Blackpin+=1
             continue
+    #for i in range(len(gegokte_code)):
+        if gegokte_code[i] in AIGokCodeComparator and (AIGokCodeComparator.count(gegokte_code[i]) > Whitepin):
+        #if gegokte_code[i] in AIGokCodeComparator and (AIGokCodeComparator.count(gegokte_code[i])- gegokte_code.count(gegokte_code[i])) >= Whitepin:
+            Whitepin += 1
+
+    '''       
     for i in range(len(gegokte_code)):
-        if gegokte_code[i] in AIGokCodeComparator:
-            if AIGokCodeComparator.count(gegokte_code[i])>  Blackpin and Whitepin < AIGokCodeComparator.count(gegokte_code[i]) and gegokte_code.count(gegokte_code[i])>Blackpin:
+        if gegokte_code[i] in AIGokCodeComparator and gegokte_code[i] !=AIGokCodeComparator[i] and AIGokCodeComparator.count(gegokte_code[i]) <=gegokte_code.count(gegokte_code[i]):
+            #if Whitepin < AIGokCodeComparator.count(gegokte_code[i]) and gegokte_code[i] !=AIGokCodeComparator[i]:
                 # hier moet ik nog aanpassen nu is het verkeerd
-                Whitepin += 1
-            else:
-                continue
+            Whitepin += 1
+        else:
             continue
+        continue
+    '''
     return [Blackpin,Whitepin]
 
+def aibraincode(mainlist):
+    ailist = mainlist
+    for i in range(len(ailist)):
+        # print(mainlist[i],end= " ")
+        AITestuitkomst = ControlleCodeAIBrain(ailist[i])
+        # print(ControlleCodeAIBrain(mainlist[i]))
+        if (AITestuitkomst[0] < GokUitkomst[0] or GokUitkomst[1] > AITestuitkomst[1]):
+            # if (AITestuitkomst[0] < GokUitkomst[0] or GokUitkomst[1] > AITestuitkomst[1]):
+            # if (AITestuitkomst[0] < GokUitkomst[0] or  GokUitkomst[1] < AITestuitkomst[1]):
+            # if (AITestuitkomst[0] < GokUitkomst[0]) or (AITestuitkomst[1] < GokUitkomst[1]):
+            # AIChoiceslist.append(mainlist[i])
+            mainlist[i] = ""
 
-GlobalGamemastercode= ['groen', 'zwart', 'zwart', 'zwart']
+    while '' in mainlist:
+        ailist.remove('')
+    return ailist
+
+
+GlobalGamemastercode= ['zwart', 'wit', 'paars', 'wit'] #['blauw', 'paars', 'groen', 'paars']
 GlobalGokcode = []
 GlobalAwsPin = []
 AantalKleuren = 5  #maximaal 6
@@ -53,7 +87,7 @@ DefaultGokCode = [ListKleurenNamen[0], ListKleurenNamen[0], ListKleurenNamen[1],
 AIGokCodeComparator= DefaultGokCode
 result = itertools.product(ListKleurenNamen, repeat=4)
 
-#GlobalGamemastercode = [ListKleurenNamen[random.randrange(AantalKleuren)], ListKleurenNamen[random.randrange(AantalKleuren)],ListKleurenNamen[random.randrange(AantalKleuren)], ListKleurenNamen[random.randrange(AantalKleuren)]]
+GlobalGamemastercode = [ListKleurenNamen[random.randrange(AantalKleuren)], ListKleurenNamen[random.randrange(AantalKleuren)],ListKleurenNamen[random.randrange(AantalKleuren)], ListKleurenNamen[random.randrange(AantalKleuren)]]
 
 mainlist = []
 AIChoiceslist= []
@@ -67,20 +101,44 @@ print("Kraak",GlobalGamemastercode)
 print("Gok  ",DefaultGokCode)
 GokUitkomst = ControlleCode(DefaultGokCode)
 print(GokUitkomst)
+print(len(mainlist))
+ronde = 1
 
+while ronde < 50:
+    mainlist = aibraincode(mainlist)
+    GokUitkomst = ControlleCode(mainlist[random.randrange(len(mainlist))])
+    print(GokUitkomst)
+    print(len(mainlist))
+    ronde +=1
+print(mainlist)
+
+#print(aibraincode(mainlist))
+
+'''
 for i in range(len(mainlist)):
     #print(mainlist[i],end= " ")
     AITestuitkomst = ControlleCodeAIBrain(mainlist[i])
     #print(ControlleCodeAIBrain(mainlist[i]))
-    if (AITestuitkomst[0] < GokUitkomst[0]) or (AITestuitkomst[1] < GokUitkomst[1]):
+    if (AITestuitkomst[0] < GokUitkomst[0] or GokUitkomst[1]-1 > AITestuitkomst[1]):
+    #if (AITestuitkomst[0] < GokUitkomst[0] or GokUitkomst[1] > AITestuitkomst[1]):
+    #if (AITestuitkomst[0] < GokUitkomst[0] or  GokUitkomst[1] < AITestuitkomst[1]):
+    #if (AITestuitkomst[0] < GokUitkomst[0]) or (AITestuitkomst[1] < GokUitkomst[1]):
         #AIChoiceslist.append(mainlist[i])
         mainlist[i] = ""
 
 while '' in  mainlist:
     mainlist.remove('')
 
+'''
+
+'''
 print(len(mainlist))
-#print(mainlist)
+print(mainlist)
+print("Kraak",GlobalGamemastercode)
+print("Gok  ",DefaultGokCode)
+print(GokUitkomst)
+print(len(mainlist))
+'''
 #ronde 2
 '''
 AIGokCodeComparator = AIChoiceslist[random.randrange(len(AIChoiceslist))]
