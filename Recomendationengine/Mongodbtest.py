63e27eb13c1ab7a9331d4ef16ef46f5a6e19054f
RichardDev01 <richard.jansen@student.hu.nl>
2020-03-03 13:09:57 +0100

    Connectie met db werkent

--- Recomendationengine/Mongodbtest.py
+++ Recomendationengine/Mongodbtest.py
@@ -5,6 +5,17 @@ import pymongo
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 
-db = client['test-database']
+#db = client['test-database']
+db = client.huwebshop
+
+col = db.profiles
+
+products = col.find()
+
+
+for x in products:
+    print(x)
 
 print(db)
+
+
