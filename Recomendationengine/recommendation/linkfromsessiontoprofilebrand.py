e014ac3b703e7ed154da8a0191b24ccb247bbc36
RichardDev01 <richard.jansen@student.hu.nl>
2020-08-05 10:04:50 +0200

    old commit

--- Recomendationengine/recommendation/linkfromsessiontoprofilebrand.py
+++ Recomendationengine/recommendation/linkfromsessiontoprofilebrand.py
@@ -1,3 +1,10 @@
+'''
+Het doel is om uit Itoder de brandnaam te halen via string manipulation
+via de brand naam een brand ID krijg
+dan een random product ID via brand ID krijgen
+Product ID weg schrijven naar profile
+'''
+
 import psycopg2
 
 
@@ -8,19 +15,14 @@ cur.execute("select prefences, profile_id from session where profile_id != 'null
 
 prefences = cur.fetchall()
 
-for prefencenid in prefences:
-    #prefence = json.loads(prefence[0])
+for prefencenid in prefences[:5]:
     prefence = prefencenid[0]
-    #print(type(prefence))
-    #print(prefence)
     conv = list(prefence.split(","))
-    #print(conv)
-    #print(type(conv))
     catlst = []
     try:
         for i in conv:
 
-            if 'category' in i:
+            if 'brand' in i:
                 i = list(i.split("{'"))
                 #i = i[0:-2]
                 #print(type(i))
@@ -28,32 +30,32 @@ for prefencenid in prefences:
                 if i[1] == "'":
                     i.pop(1)
                 catlst.append(i[1][0:-3])
+                catlst.append(i[2][0:-3])
                 #print(catlst)
                 #print(i, end= "  ")
+        if catlst[0] == 'brand':
+            catlst.pop(0)
         recom = list(prefencenid)+catlst
         recom.pop(0)
-        #print(recom)
 
-        for j in recom:
-            if str(j).__contains__("views"):
-                recom.remove(j)
+        if recom[1] == 'brand':
+            recom.pop(1)
+        print(recom)
+        try:
+            cur.execute("select idbrand from brand where brandnaam LIKE '{}'".format(recom[1]))
+            #cur.execute("select idbrand from brand where brandnaam LIKE '{}';".format('Zonnatura'))
+        except:
+            print("error 2")
 
-        if len(recom) >=4:
-            cur.execute("select idcategory from category where category = '{}' and sub_category = '{}' and sub_sub_category ='{}'".format(recom[1],recom[2],recom[3]))
-        elif len(recom) == 3:
-            cur.execute("select idcategory from category where category = '{}' and sub_category = '{}'".format(recom[1], recom[2]))
-        elif len(recom) == 2:
-            cur.execute("select idcategory from category where category = '{}'".format(recom[1]))
-        elif len(recom) == 1:
-            cur.execute("select idcategory from category where category IS NULL")
+
         reccatid = cur.fetchall()
-        #print(reccatid[0][0])
+        print(reccatid[0][0])
         recom.append(reccatid[0][0])
         #print(recom[4])
 
         try:
             if len(recom) > 1:
-                cur.execute("SELECT id FROM product where category_idcategory = {} ORDER BY RANDOM() LIMIT 1;".format(recom[-1]))
+                cur.execute("SELECT id FROM product where brand_idbrand = {} ORDER BY RANDOM() LIMIT 1;".format(recom[1]))
                 prodid= cur.fetchall()
                 print(prefencenid[1],prodid[0][0])
                 recommendation = str(prodid[0][0])
